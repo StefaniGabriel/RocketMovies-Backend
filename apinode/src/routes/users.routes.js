006b74c5fe1b9bafb159aa4678103ee33163c27a
Stefani Gabriel <111387502+StefaniGabriel@users.noreply.github.com>
2023-01-03 12:43:45 -0300

    Add files via upload

---
+++ apinode/src/routes/users.routes.js
@@ -0,0 +1,15 @@
+const { Router, response }  =  require("express");
+
+const UsersControllers = require("../controllers/UsersControllers");
+
+const usersRoutes = Router();
+
+const usersControllers = new UsersControllers();
+
+usersRoutes.post("/", usersControllers.create);
+usersRoutes.put("/:id", usersControllers.update);
+
+
+
+
+module.exports = usersRoutes;
